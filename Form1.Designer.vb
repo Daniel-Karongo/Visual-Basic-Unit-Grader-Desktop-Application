50ed1367b32797036b801c8c28758442c7f0112e
Daniel-Karongo <danieltorrent001@gmail.com>
2024-12-04 09:53:19 +0300

    First Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtReg = New System.Windows.Forms.TextBox()
+        Me.TxtName = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TxtCAT1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TxtCAT2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TxtExam = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.LblErrors = New System.Windows.Forms.Label()
+        Me.LblExamHints = New System.Windows.Forms.Label()
+        Me.MarkList = New System.Windows.Forms.DataGridView()
+        CType(Me.MarkList, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(14, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(709, 34)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Unit Grader"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(37, 90)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Reg No:"
+        '
+        'TxtReg
+        '
+        Me.TxtReg.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtReg.Location = New System.Drawing.Point(105, 87)
+        Me.TxtReg.Name = "TxtReg"
+        Me.TxtReg.Size = New System.Drawing.Size(216, 22)
+        Me.TxtReg.TabIndex = 2
+        '
+        'TxtName
+        '
+        Me.TxtName.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtName.Location = New System.Drawing.Point(444, 87)
+        Me.TxtName.Name = "TxtName"
+        Me.TxtName.Size = New System.Drawing.Size(254, 22)
+        Me.TxtName.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(352, 88)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 16)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Full Name:"
+        '
+        'TxtCAT1
+        '
+        Me.TxtCAT1.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtCAT1.Location = New System.Drawing.Point(105, 178)
+        Me.TxtCAT1.Name = "TxtCAT1"
+        Me.TxtCAT1.Size = New System.Drawing.Size(216, 22)
+        Me.TxtCAT1.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(37, 181)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 16)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "CAT 1:"
+        '
+        'TxtCAT2
+        '
+        Me.TxtCAT2.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtCAT2.Location = New System.Drawing.Point(105, 210)
+        Me.TxtCAT2.Name = "TxtCAT2"
+        Me.TxtCAT2.Size = New System.Drawing.Size(216, 22)
+        Me.TxtCAT2.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(37, 213)
+        Me.Label5.Name = "Label5"
+        Me.Label5.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label5.Size = New System.Drawing.Size(61, 18)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "CAT 2:"
+        '
+        'TxtExam
+        '
+        Me.TxtExam.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtExam.Location = New System.Drawing.Point(444, 178)
+        Me.TxtExam.Name = "TxtExam"
+        Me.TxtExam.Size = New System.Drawing.Size(254, 22)
+        Me.TxtExam.TabIndex = 10
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(352, 181)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(85, 16)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Main Exam:"
+        '
+        'Label7
+        '
+        Me.Label7.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(14, 54)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(709, 20)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Student Details"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label8
+        '
+        Me.Label8.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(21, 122)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(702, 20)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "Student's Marks"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(260, 247)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(105, 28)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(372, 247)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(105, 28)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'LblErrors
+        '
+        Me.LblErrors.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblErrors.ForeColor = System.Drawing.Color.Red
+        Me.LblErrors.Location = New System.Drawing.Point(21, 151)
+        Me.LblErrors.Name = "LblErrors"
+        Me.LblErrors.Size = New System.Drawing.Size(702, 18)
+        Me.LblErrors.TabIndex = 16
+        Me.LblErrors.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LblExamHints
+        '
+        Me.LblExamHints.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblExamHints.ForeColor = System.Drawing.Color.Blue
+        Me.LblExamHints.Location = New System.Drawing.Point(352, 212)
+        Me.LblExamHints.Name = "LblExamHints"
+        Me.LblExamHints.Size = New System.Drawing.Size(367, 19)
+        Me.LblExamHints.TabIndex = 17
+        Me.LblExamHints.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'MarkList
+        '
+        Me.MarkList.BackgroundColor = System.Drawing.SystemColors.Control
+        Me.MarkList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.MarkList.GridColor = System.Drawing.SystemColors.Control
+        Me.MarkList.Location = New System.Drawing.Point(41, 289)
+        Me.MarkList.Name = "MarkList"
+        Me.MarkList.Size = New System.Drawing.Size(657, 183)
+        Me.MarkList.TabIndex = 18
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(737, 490)
+        Me.Controls.Add(Me.MarkList)
+        Me.Controls.Add(Me.LblExamHints)
+        Me.Controls.Add(Me.LblErrors)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TxtExam)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TxtCAT2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TxtCAT1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TxtName)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TxtReg)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Exam Grader"
+        CType(Me.MarkList, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxtReg As TextBox
+    Friend WithEvents TxtName As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TxtCAT1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TxtCAT2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TxtExam As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents LblErrors As Label
+    Friend WithEvents LblExamHints As Label
+    Friend WithEvents MarkList As DataGridView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,236 @@
+﻿Public Class Form1
+    ' Method that specifies the layout of the datagridview as:
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' The datagridview has 4 columns
+        MarkList.ColumnCount = 4
+        MarkList.Columns(0).Name = "Registration Number"
+        MarkList.Columns(1).Name = "Full Name"
+        MarkList.Columns(2).Name = "Unit Mark"
+        MarkList.Columns(3).Name = "Grade"
+
+        ' The widths of the columns are as:
+        MarkList.Columns(0).Width = 160
+        MarkList.Columns(1).Width = 200
+        MarkList.Columns(2).Width = 120
+        MarkList.Columns(3).Width = 100
+
+    End Sub
+
+    ' Method that specifies what happens when the "Clear" button is clicked: it clears all the TextBoxes
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TxtReg.Clear()
+        TxtName.Clear()
+        TxtCAT1.Clear()
+        TxtCAT2.Clear()
+        TxtExam.Clear()
+    End Sub
+
+    ' Method that specifies what happens when the "CAT1" TextBox gains the keyboard's focus: a Hint shows on the "LblExamHints" Label as:
+    Private Sub TxtCAT1_FocusGained(sender As Object, e As EventArgs) Handles TxtCAT1.GotFocus
+        LblExamHints.Text = "Out of 20. Both CATs Make Up 40% Of The Overall Mark"
+    End Sub
+    ' Method that specifies what happens when the "CAT1" TextBox loses the keyboard's focus: the Hint is cleared
+    Private Sub TxtCAT1_FocusLost(sender As Object, e As EventArgs) Handles TxtCAT1.LostFocus
+        LblExamHints.Text = ""
+    End Sub
+
+    ' Method that specifies what happens when the "CAT2" TextBox gains the keyboard's focus: a Hint shows on the "LblExamHints" Label as:
+    Private Sub TxtCAT2_FocusGained(sender As Object, e As EventArgs) Handles TxtCAT2.GotFocus
+        LblExamHints.Text = "Out of 20. Both CATs Make Up 40% Of The Overall Mark"
+    End Sub
+
+    Private Sub TxtCAT2_FocusLost(sender As Object, e As EventArgs) Handles TxtCAT2.LostFocus
+        ' Method that specifies what happens when the "CAT2" TextBox loses the keyboard's focus: the Hint is cleared
+        LblExamHints.Text = ""
+    End Sub
+
+    ' Method that specifies what happens when the "Main Exam" TextBox gains the keyboard's focus: a Hint shows on the "LblExamHints" Label as:
+    Private Sub TxtExam_FocusGained(sender As Object, e As EventArgs) Handles TxtExam.GotFocus
+        LblExamHints.Text = "Out of 60. This Makes Up 60% Of The Overall Mark"
+    End Sub
+
+    ' Method that specifies what happens when the "Main Exam" TextBox loses the keyboard's focus: the Hint is cleared
+    Private Sub TxtExam_FocusLost(sender As Object, e As EventArgs) Handles TxtExam.LostFocus
+        LblExamHints.Text = ""
+    End Sub
+
+    ' Method that restricts the input into the "CAT1" TextBox as:
+    Private Sub TxtCAT1_TextChanged(sender As Object, e As KeyPressEventArgs) Handles TxtCAT1.KeyPress
+
+        If Char.IsNumber(e.KeyChar) = False And e.KeyChar <> "." And e.KeyChar <> ControlChars.Back Then
+            ' If clause that ensures that the program flows only when the keyboard key pressed is a digit or a period(.)
+
+            ' Display the Error Message:
+            LblErrors.Text = "Only numbers or a Period (.) are Allowed"
+            ' Beep
+            System.Media.SystemSounds.Beep.Play()
+            ' Reject the input
+            e.Handled = True
+
+        ElseIf e.KeyChar = "." And TxtCAT1.Text.Contains(".") Then
+            ' If clause that ensures that the program flows only when the keyboard key pressed is the first period, if the period key (.) is pressed
+
+            ' Display the Error Message:
+            LblErrors.Text = "Only one Period (.) is Allowed"
+            ' Beep
+            System.Media.SystemSounds.Beep.Play()
+            ' Reject the input
+            e.Handled = True
+
+        Else
+            ' If clause that ensures that the program flows only when the keyboard key pressed is represents a valid character
+
+        End If
+    End Sub
+
+    ' Method that restricts the input into the "CAT2" TextBox as:
+    Private Sub TxtCAT2_TextChanged(sender As Object, e As KeyPressEventArgs) Handles TxtCAT2.KeyPress
+
+        If Char.IsNumber(e.KeyChar) = False And e.KeyChar <> "." And e.KeyChar <> ControlChars.Back Then
+            ' If clause that ensures that the program flows only when the keyboard key pressed is a digit or a period(.)
+
+            ' Display the Error Message:
+            LblErrors.Text = "Only numbers or a Period (.) Allowed"
+            ' Beep
+            System.Media.SystemSounds.Beep.Play()
+            ' Reject the input
+            e.Handled = True
+
+        ElseIf e.KeyChar = "." And TxtCAT2.Text.Contains(".") Then
+            ' If clause that ensures that the program flows only when the keyboard key pressed is the first period, if the period key (.) is pressed
+
+            ' Display the Error Message:
+            LblErrors.Text = "Only one Period (.) is Allowed"
+            ' Beep
+            System.Media.SystemSounds.Beep.Play()
+            ' Reject the Input
+            e.Handled = True
+
+        Else
+            ' If clause that ensures that the program flows only when the keyboard key pressed is represents a valid character
+            LblErrors.Text = ""
+
+        End If
+    End Sub
+
+    ' Method that restricts the input into the "Main Exam" TextBox as:
+    Private Sub TxtExam_TextChanged(sender As Object, e As KeyPressEventArgs) Handles TxtExam.KeyPress
+
+        If Char.IsNumber(e.KeyChar) = False And e.KeyChar <> "." And e.KeyChar <> ControlChars.Back Then
+            ' If clause that ensures that the program flows only when the keyboard key pressed is a digit or a period(.)
+
+            ' Display the Error Message:
+            LblErrors.Text = "Only numbers or a Period (.) Allowed"
+            ' Beep
+            System.Media.SystemSounds.Beep.Play()
+            ' Reject the input
+            e.Handled = True
+
+        ElseIf e.KeyChar = "." And TxtExam.Text.Contains(".") Then
+            ' If clause that ensures that the program flows only when the keyboard key pressed is the first period, if the period key (.) is pressed
+
+            'Display the Error Message:
+            LblErrors.Text = "Only one Period (.) is Allowed"
+            'Beep
+            System.Media.SystemSounds.Beep.Play()
+            'Reject the input
+            e.Handled = True
+
+        Else
+            ' If clause that ensures that the program flows only when the keyboard key pressed is represents a valid character
+            LblErrors.Text = ""
+
+        End If
+    End Sub
+
+    ' Method that computes the grade of each student then displays it, together with the student's details and total mark, in a table  
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Variables to store what is typed in
+
+        'String variables to store the each student's details (Name and Registation Nmber)
+        Dim regNum As String = TxtReg.Text
+        Dim stdName As String = TxtName.Text
+
+        'Double variables to store the each student's CAT marks and Main Exam mark
+        Dim cat1 As Double = Val(TxtCAT1.Text)
+        Dim cat2 As Double = Val(TxtCAT2.Text)
+        Dim mainExam As Double = Val(TxtExam.Text)
+
+        ' If clause that ensures that the program flows only when all the fields are filled
+        If ((TxtReg.Text = "") = False) AndAlso ((TxtName.Text = "") = False) AndAlso
+            ((TxtCAT1.Text = "") = False) AndAlso ((TxtCAT2.Text = "") = False) AndAlso
+            ((TxtExam.Text = "") = False) Then
+
+            ' If clause that ensures that the program flows only when the marks provided for each assessement (CATs and Main Exam) are each in the required range as:
+
+            'The mark for CAT1 should be less than or equal to 20,
+            'The mark for CAT2 should also be less than or equal to 20,
+            'The mark for the main exam should be less than or equal to 60,
+            If (cat1 <= 20) And (cat2 <= 20) And (mainExam <= 60) Then
+
+                'The sum of the two CAT Marks is evaluated as: CAT1 + CAT2
+                Dim catOverall As Double = cat1 + cat2
+                ' A Double variable is declared to store the value of the overall mark (CATS + Main Exam)
+                Dim finalMark As Double
+                'The Overall Mark is calculated and its value assigned to the "finalmark" variable
+                finalMark = catOverall + mainExam
+
+                ' A character variable is declared to store the value of each student's grade
+                Dim grade As Char
+
+                ' The Grades are determined as:
+                '0-39 = "E", 40-49 = "D", 50-59 = "C", 60-69 = "B", 70-100 = "A",
+
+                'Consider the value stored in the "grade variable": 
+                Select Case finalMark
+
+                    ' If it is greater than or equal to 70
+                    Case >= 70
+                        ' Let the student's grade be "A" and assign it to the "grade" variable 
+                        grade = "A"
+                    ' If it is greater than or equal to 60
+                    Case >= 60
+                        ' Let the student's grade be "B" and assign it to the "grade" variable
+                        grade = "B"
+                    ' If it is greater than or equal to 50
+                    Case >= 50
+                        ' Let the student's grade be "C" and assign it to the "grade" variable
+                        grade = "C"
+                    ' If it is greater than or equal to 40
+                    Case >= 40
+                        ' Let the student's grade be "D" and assign it to the "grade" variable
+                        grade = "D"
+                        ' If it less than 40
+                    Case Else
+                        ' Let the student's grade be "E" and assign it to the "grade" variable
+                        grade = "E"
+
+                End Select
+
+                ' A record is added to the datagridview (Table) containing:
+                ' The Registration Number, Name, Overall Mark and Grade of the Student as:
+
+                'A String Array named as "row" is initialised and assigned the values: Registration Number, Name, Overall Mark and Grade of the Student 
+                Dim row As String() = New String() {regNum.ToString, stdName.ToString, finalMark.ToString, grade.ToString}
+
+                ' The "row" array is added to the datagridview as a record where each value in the array is placed on its own row and column
+                MarkList.Rows.Add(row)
+
+            Else
+                ' If the mark entered for either a CATs or the Main Exam is outside the required range
+
+                'Beep
+                System.Media.SystemSounds.Beep.Play()
+                ' Display the Error message
+                LblErrors.Text = "Invalid Marks. Please Re-enter Them"
+            End If
+        Else
+            ' If the there is a field that has not been filled
+
+            'Beep
+            System.Media.SystemSounds.Beep.Play()
+            ' Display the Error message
+            LblErrors.Text = "Please Fill In All The Details"
+        End If
+    End Sub
+End Class
